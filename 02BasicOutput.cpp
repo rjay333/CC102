ed8938a9de8205297a98de92a9060cb101bd379f
RJAY POGI <painaganjocelyn1@gmail.com>
2024-10-03 10:03:29 +0800

    Add files via upload

---
+++ 02BasicOutput.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main ()
+{
+	cout<< "School\t\t\t	:	Our Lady of Lourdes College Foundation, INC." << endl;
+	cout<< "Department\t\t\t:	OLLCF College of Computer Studies" << endl;
+	cout<< "Course & Year Level\t\t:	BSIT 1-A" << endl;
+	cout<< "Full Name\t\t	:	R-jay Painagan" << endl;
+	cout<< "Address\t\t\t	:	Harmony village 2 brgy. mangcruz Daet, Camarines Norte" << endl;
+	
+return 0;
+}
