8a83fa158929510e9987db64f6cb0a01af446eb6
RJAY POGI <painaganjocelyn1@gmail.com>
2024-10-08 08:51:34 +0800

    Add files via upload

---
+++ 03variableDeclaration.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int age;
+	
+	// ask for user's age
+	cout << "Enter Your Age: ";
+	cin >> age;
+	
+	// print THANK YOU MESSAGE
+	cout << "THANK YOU!" << endl;
+	
+	return 0;
+}
