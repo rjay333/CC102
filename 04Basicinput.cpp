59ea04f3fd2fb7b57203ee484b2b11309b5c222c
RJAY POGI <painaganjocelyn1@gmail.com>
2024-10-08 09:15:02 +0800

    Add files via upload

---
+++ 04Basicinput.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    string nickname, school;
+	;int course;
+	
+	cout << " Nickname : ";
+	cin >> nickname;
+	
+	cout << " Course - Year Level : ";
+	cin >> course;
+	
+	cout << " School : " ;
+	cin >> school;
+	
+	return 0;
+}
